c9c7ad269e261402c93f88080b981886808467b8
Nadeem Fayaz <iamabnadeem99@gmail.com>
2023-12-10 22:50:30 +0530

    new array

--- src/App.js
+++ src/App.js
@@ -69,7 +69,7 @@ function Item({ item }) {
   return (
     <li>
       <span style={item.packed ? {textDecoration:'line-through'}:{} }> {item.quantity} {item.description}</span>
-      <button>❌</button>
+      <button>❌</button> 
     </li>
   );
 }
